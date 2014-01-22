afc40cdf6c4745e5b58b6001eccaa6579a3613a2
DPL-Alpha <DPL-Alphas-iMac@DPL-Alphas-iMacs-iMac.local>
2014-01-21 19:13:11 -0700

    Creates Encryptor project. Adds cipher, adds encrypt_letter method, adds encrypt_string

---
+++ encryptor.rb
@@ -0,0 +1,23 @@
+class Encryptor
+  def cipher
+  {'a' => 'n','b' => 'o','c' => 'p','d' => 'q',
+   'e' => 'r','f' => 's','g' => 't','h' => 'u',
+   'i' => 'v','j' => 'w','k' => 'x','l' => 'y',
+   'm' => 'z','n' => 'a','o' => 'b','p' => 'c',
+   'q' => 'd','r' => 'e','s' => 'f','t' => 'g',
+   'u' => 'h','v' => 'i','w' => 'j','x' => 'k',
+   'y' => 'l','z' => 'm', ' ' => ' '}
+  end
+
+  def encrypt_string(string)
+    letters = string.split('')
+      encrypted_letters = letters.collect do |letter|
+        encrypt_letter(letter)
+      end
+      encrypted_letters.join
+    end
+
+  def encrypt_letter(letter)
+    cipher[letter.downcase]
+  end
+end
